1268fe0afc3f004580bc7b90330038f70733f47e
Adam Stępień <vesemir2k@gmail.com>
2024-03-07 15:56:08 +0100

    Add Prettier configuration

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
-import { create, all } from "mathjs";
+import { create, all } from 'mathjs';
 const math = create(all, {});
 
-const keys = document.querySelectorAll<HTMLElement>(".keypad button");
-const dot = document.querySelector(".toggle") as HTMLElement;
-const toggleBtn = document.querySelectorAll<HTMLElement>(".toggle div");
-const screenEl = document.querySelector(".screen") as HTMLInputElement;
-let operation: string = "";
+const keys = document.querySelectorAll<HTMLElement>('.keypad button');
+const dot = document.querySelector('.toggle') as HTMLElement;
+const toggleBtn = document.querySelectorAll<HTMLElement>('.toggle div');
+const screenEl = document.querySelector('.screen') as HTMLInputElement;
+let operation: string = '';
 
-keys.forEach((key) => {
-  key.addEventListener("click", () => {
+keys.forEach(key => {
+  key.addEventListener('click', () => {
     const valueKey = key.dataset.key;
 
-    if (valueKey === "=") {
+    if (valueKey === '=') {
       let result: number = +math.evaluate(operation);
       return (screenEl.value = result.toFixed(3));
-    } else if (valueKey === "RESET") {
-      operation = "";
-    } else if (valueKey === "DEL") {
+    } else if (valueKey === 'RESET') {
+      operation = '';
+    } else if (valueKey === 'DEL') {
     } else {
       operation += valueKey;
     }
@@ -26,19 +26,19 @@ keys.forEach((key) => {
 });
 
 toggleBtn.forEach((btn, index) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener('click', () => {
     if (index === 1) {
-      document.documentElement.setAttribute("data-theme", "light");
-      dot.classList.remove("theme-purple");
-      dot.classList.add("theme-light");
+      document.documentElement.setAttribute('data-theme', 'light');
+      dot.classList.remove('theme-purple');
+      dot.classList.add('theme-light');
     } else if (index === 2) {
-      document.documentElement.setAttribute("data-theme", "purple");
-      dot.classList.remove("theme-light");
-      dot.classList.add("theme-purple");
+      document.documentElement.setAttribute('data-theme', 'purple');
+      dot.classList.remove('theme-light');
+      dot.classList.add('theme-purple');
     } else {
-      document.documentElement.removeAttribute("data-theme");
-      dot.classList.remove("theme-purple");
-      dot.classList.remove("theme-light");
+      document.documentElement.removeAttribute('data-theme');
+      dot.classList.remove('theme-purple');
+      dot.classList.remove('theme-light');
     }
   });
 });
